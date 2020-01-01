4e30d545bae07418c2419a42465703d541680568
David Beswick <dlbeswick@gmail.com>
2020-01-02 00:22:18 +1100

    Copyrights

--- app/ts/instrument.ts
+++ app/ts/instrument.ts
@@ -1,3 +1,24 @@
+/*
+	เครื่องฉิ่ง / Kreuung Ching
+  This file is part of the Automatic Ching program for practicing
+  Thai music.
+  
+  Copyright (C) 2019 David Beswick <dlbeswick@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 /// <reference path="shaper.ts" />
 namespace AppChing {
   export abstract class Instrument {
--- app/ts/parser-iterativestart-gc-optimised.js
+++ app/ts/parser-iterativestart-gc-optimised.js
@@ -1,4 +1,25 @@
 /*
+	เครื่องฉิ่ง / Kreuung Ching
+  This file is part of the Automatic Ching program for practicing
+  Thai music.
+  
+  Copyright (C) 2019 David Beswick <dlbeswick@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
 Input size 6900
 tokenize average over 1000 runs: 1.2167832167832169 ms
 parse + semantic average over 1000 runs: 2.2217782217782216 ms
