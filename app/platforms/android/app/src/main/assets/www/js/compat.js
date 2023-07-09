d3e69cc0a3ad80fc2d7c254636e1bfe850793361
David Beswick <dlbeswick@gmail.com>
2023-07-09 14:22:38 +0700

    Remove generated files from repo as F-Droid now has a solution

--- app/platforms/android/app/src/main/assets/www/js/compat.js
+++
@@ -1,7 +0,0 @@
-if (!Array.prototype.flatMap) {
-  Object.defineProperty(Array.prototype, 'flatMap', {
-    value: function(fn) {
-      return this.map(fn).reduce((acc, val) => acc.concat(val), []);
-    }
-  })
-}
